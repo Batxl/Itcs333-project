6cf405f46b578e826f4eb2ca044081599375379c
Batool <155333963+Batxl@users.noreply.github.com>
2024-12-09 02:17:47 +0300

    include db_connection

--- comments.php
+++ comments.php
@@ -17,12 +17,7 @@
 </style>
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "project";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+include 'db_connection.php';
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
