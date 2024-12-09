37209adec3bc92eec0d77593261bd0aa3dbe2a7a
ZainabSami-04 <zai04nab@gmail.com>
2024-12-09 20:46:54 +0300

    delete room

--- deleteroom.php
+++ deleteroom.php
@@ -2,19 +2,18 @@
 
 include 'admin-database.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $room_id_to_delete = $_POST['room-id'];
 
     try {
-        // SQL query to delete a room
-        $sql = "DELETE FROM room WHERE ID = :room_id";
+
+        $sql = "DELETE FROM reservations WHERE room_id = :room_id";  
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':room_id', $room_id_to_delete);
         $stmt->execute();
 
         echo "Room deleted successfully!";
-        header("Location: manage.php"); 
+        header("Location: manage.php");  
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
