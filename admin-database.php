414affebff935eba1832dff5730962587e06c568
ZainabSami-04 <zai04nab@gmail.com>
2024-12-10 18:45:21 +0300

    db

--- admin-database.php
+++ admin-database.php
@@ -1,17 +1,16 @@
 <?php
-
-$servername = "localhost"; 
+$servername = "localhost";
 $username = "root";
 $password = ""; 
-$dbname = "project";
+$dbname = "project"; 
 
-try {
-    // Create a PDO connection
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit; 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully";
 ?>
