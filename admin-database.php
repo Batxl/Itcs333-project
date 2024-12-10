c0d149531904c5dbadd6d5d5ae09444aa0a38e6a
ZainabSami-04 <zai04nab@gmail.com>
2024-12-10 18:23:00 +0300

    db

--- admin-database.php
+++ admin-database.php
@@ -1,16 +1,18 @@
 <?php
 
-$servername = "localhost";
+$servername = "127.0.0.1"; 
 $username = "root";
 $password = ""; 
 $dbname = "project";
 
 try {
     // Create a PDO connection
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     
 } catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-    exit;  // Stop execution if the connection fails
+    echo "Connection failed: " . $e->getMessage();
+    exit; 
 }
 ?>
+
