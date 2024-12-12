5f9c50717b4bb5458957a7eb048c0f36a65e50d9
ZainabSami-04 <zai04nab@gmail.com>
2024-12-12 09:03:25 +0300

    update

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 // Redirect based on user role
                 if (strpos($email, '@stu.uob.edu.bh') !== false) {
-                    header("Location:http://localhost/PROJECT333/index.php");
+                    header("Location:http://localhost/11/index.php");
                     exit();
                 } elseif (strpos($email, '@admin.uob.edu.bh') !== false) {
                     header("Location: http://localhost/PROJECT333/admin-dashboard.php");
